64f04b46277227ea072d479211c8c6cafc1812d3
joker153 <77005994+joker153@users.noreply.github.com>
2023-04-10 16:07:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/joker153/Evamari12.git /Evamari12
+  git clone https://github.com/joker153/TOVINOXAUTOFILTERBOT.git /TOVINOXAUTOFILTERBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Evamari12
+  git clone $UPSTREAM_REPO /TOVINOXAUTOFILTERBOT
 fi
-cd /Evamari12
+cd /TOVINOXAUTOFILTERBOT
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting TOVINOXAUTOFILTERBOT...."
 python3 bot.py
